e81a90bfdc9508c5f5ffc796b63e4315ee917408
dantesouz <149194984+dantesouz@users.noreply.github.com>
2023-11-27 17:17:33 -0300

    Estruturação do layout

---
+++ src/components/Rodape.js
@@ -0,0 +1,12 @@
+import { Col } from 'reactstrap';
+
+function Rodape() {
+    return (
+        <div>
+            <Col className='bg-dark text-light text-center py-5'>
+                <spam>&copy; CodeChef - Todos os direitos reservados </spam>
+            </Col>
+        </div>
+    )
+}
+export default Rodape;
---
+++ src/pages/Cardapio.js
@@ -0,0 +1,8 @@
+function Cardapio(){
+    return(
+        
+        <h1>Página Cardapio</h1>
+        
+    )
+}
+export default Cardapio
